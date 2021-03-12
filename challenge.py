7dcdc49ed38a7f55e466c3bbd59c92d6e6347fc5
benjamin.oluyomi <boluyomi@academytrainee.com>
2021-03-12 11:15:55 +0000

    fixed variables error

--- challenge.py
+++ challenge.py
@@ -8,11 +8,11 @@ def multiples():
             print(x)
     print("The following are multiples of 8 up to 200")
     for x in range (8,200):
-        if x%y == 0:
+        if x%z == 0:
             print(x)
     print("The following are multiples of 9 up to 300")
     for x in range (9,300):
-        if x%y == 0:
+        if x%c == 0:
             print(x)
 
 
